6ca5777d28a75aba44d4f4c86c4f2c714ff08f9d
LeydiHuallpa <leydihuallpa67@gmail.com>
2020-01-14 13:30:14 -0500

    Diseño de la app, general

--- app/src/main/java/com/example/tarea1unidad01_leydihuallpa/MainActivity.java
+++ app/src/main/java/com/example/tarea1unidad01_leydihuallpa/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tarea1unidad01_leydihuallpa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void ejercicio1(View view){
+
+        startActivity(new Intent(this, Calculadora.class));
+    }
+
+    public void ejercicio2(View view){
+
+        startActivity(new Intent(this, Formulario.class));
+    }
+    public void salir(View view){
+        finish();
+    }
+
 }
